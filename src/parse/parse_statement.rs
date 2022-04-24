977018fb8ceb0414aaf1e2e3974d2807c7ee6433
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-04-24 23:45:26 +0100

    Copied the old one but modified it to use lines

--- src/parse/parse_statement.rs
+++ src/parse/parse_statement.rs
@@ -1,6 +1,7 @@
 use crate::{
     error::{ Error, ErrorKind },
     token::Token,
+    line::Line,
     parse::{ Expr, parse_expression, TypeKind }
 };
 
@@ -57,24 +58,17 @@ pub struct Statement<'s, 't> {
 
 
 impl<'s, 't> Statement<'s, 't> {
-    pub fn from_tokens(tokens: &'t [Token<'s>], position: (usize, usize)) -> Result<Self, Vec<Error>> {
+    pub fn from_line(line: &Line<'s, 't>) -> Result<Self, Vec<Error>> {
         /* Creates a statement from a list of tokens */
-
-        if tokens.is_empty() {
-            return Error::new(ErrorKind::SyntaxError)
-                    .set_position(position)
-                    .set_message("Attempt to parse empty statement")
-                    .into();
-        }
     
         for option in PARSE_OPTIONS {
-            if let Some(res) = option(tokens) {
+            if let Some(res) = option(line) {
                 return res;
             }
         }
     
         Error::new(ErrorKind::SyntaxError)
-            .set_position(tokens[0].position())
+            .set_position(line.line_tokens[0].position())
             .set_message("Unrecognised syntax in statement")
             .into()
     }
@@ -86,13 +80,22 @@ impl<'s, 't> Statement<'s, 't> {
 type ParseOption<'s, 't> = Option<Result<Statement<'s, 't>, Vec<Error>>>;
 
 
-fn parse_declare<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_declare<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses a value declaration */
+
+    let tokens = line.line_tokens;
 
     let constant = match tokens[0].to_string().as_str() {
         s@("let"|"var") => s == "let",
         _ => return None
     };
+
+    if line.line_derivs.len() > 1 {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(line.line_derivs[0].line_tokens[0].position())
+                        .set_message("Declarations cannot have a derivative block")
+                        .into());
+    }
 
     if tokens.len() < 4 {
         return Some(Error::new(ErrorKind::SyntaxError)
@@ -165,8 +168,19 @@ fn parse_declare<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
 }
 
 
-fn parse_assignment<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_assignment<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses a value assignment */
+
+//    println!("{:#?}", line);
+
+    let tokens = line.line_tokens;
+
+    if line.line_derivs.len() != 0 {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(line.line_derivs[0].line_tokens[0].position())
+                        .set_message("Assignments cannot have a derivative block")
+                        .into());
+    }
 
     let mut eq_index = 0;
     let mut errors = Vec::new();
@@ -207,41 +221,26 @@ fn parse_assignment<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
 }
 
 
-fn parse_conditional<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_conditional<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses an if/elif/while stmt */
 
+    let tokens = line.line_tokens;
     let mut errors: Vec<Error> = Vec::new();
 
     let conditional_type = match tokens[0].to_string().as_str() {
-        "if"|"elif"|"else" => tokens[0].to_string(),
+        "if"|"elif"|"while" => tokens[0].to_string(),
         _ => return None
     };
 
-    let format_err = Error::new(ErrorKind::SyntaxError)
-                        .set_position(tokens[0].position())
-                        .set_message(
-                            format!(
-                                "Expected syntax '{} (<condition>) {{<body>}}",
-                                conditional_type
-                            )
-                        );
-
-    if tokens.len() != 3 {
-        return Some(format_err.into());
+    if line.line_derivs.is_empty() {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(line.line_tokens.last().unwrap().position())
+                        .set_message("Conditional statements must have a block")
+                        .into());
     }
 
-
-    let mut condition = parse_expression(&tokens[1..2], tokens[1].position());
-    let mut block = match &tokens[2] {
-        Token::Block { contents, .. } => {
-            if tokens[2].delim_equals("{") {
-                parse_block(contents)
-            } else {
-                format_err.into()
-            }
-        },
-        _ => format_err.into()
-    };
+    let mut condition = parse_expression(&tokens[1..], tokens[1].position());
+    let mut block = parse_statement_block(&line.line_derivs);
 
     if let Err(ref mut es) = condition {
         errors.append(es);
@@ -270,43 +269,38 @@ fn parse_conditional<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
                 is_if: conditional_type == "if"
             },
             first_token: &tokens[0],
-            last_token:  &tokens[2]
+            last_token:  line.line_derivs.last().unwrap().line_tokens.last().unwrap() // vile
         })
     })
 }
 
 
-fn parse_else<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_else<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses an else statement */
+
+    let tokens = line.line_tokens;
 
     if tokens[0].to_string() != "else" {
         return None;
     }
 
-    let format_err = Error::new(ErrorKind::SyntaxError)
+    if tokens.len() != 1 {
+        return Some(Error::new(ErrorKind::SyntaxError)
                         .set_position(tokens[0].position())
-                        .set_message("Expected syntax 'else {<body>}");
-
-    if tokens.len() != 2 {
-        return Some(format_err.into());
+                        .set_message("Expected syntax 'else {<body>}")
+                        .into());
+    } else if line.line_derivs.is_empty() {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(line.line_tokens.last().unwrap().position())
+                        .set_message("Conditional statements must have a block")
+                        .into());
     }
 
-    let block_res = match &tokens[1] {
-        Token::Block { contents, .. } => {
-            if tokens[1].delim_equals("{") {
-                parse_block(contents)
-            } else {
-                format_err.into()
-            }
-        },
-        _ => format_err.into()
-    };
-
-    Some(match block_res {
+    Some(match parse_statement_block(&line.line_derivs) {
         Ok(block) => Ok(Statement {
             stmt_type: StatementType::Else { block },
             first_token: &tokens[0],
-            last_token:  &tokens[1]
+            last_token:  line.line_derivs.last().unwrap().line_tokens.last().unwrap()
         }),
 
         Err(es) => Err(es)
@@ -314,53 +308,41 @@ fn parse_else<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
 }
 
 
-fn parse_for_loop<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_for_loop<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses a for loop */
 
-    if tokens[0].to_string() != "for" {
-        return None;
-    }
-
+    let tokens = line.line_tokens;
     let format_err = Error::new(ErrorKind::SyntaxError)
                         .set_position(tokens[0].position())
-                        .set_message("Expected syntax 'for (<iterator> in <range>) {<body>}");
-
-    if tokens.len() != 3 {
-        return Some(format_err.into());
-    }
-
-    let inner_tokens = match &tokens[1] {
-        Token::Block { open_delim, contents, .. } => {
-            if *open_delim == "(" && contents.len() >= 3 {
-                contents
-            } else {
-                return Some(format_err.into());
-            }
-        },
-
-        _ => return Some(format_err.into())
-    };
-
-    let mut block = match &tokens[2] {
-        Token::Block { open_delim, contents, .. } => {
-            if *open_delim == "{" {
-                parse_block(&contents[..])
-            } else {
-                return Some(format_err.into());
-            }
-        },
-
-        _ => return Some(format_err.into())
-    };
+                        .set_message("Expected syntax 'for <iterator> in <range>");
 
     let mut errors = Vec::new();
 
-    if errors.is_empty() && inner_tokens[1].to_string() != "in" {
+    if tokens[0].to_string() != "for" {
+        return None;
+    } else if tokens.len() < 4 {
+        errors.push(format_err.clone());
+    } else if line.line_derivs.len() == 0 {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(line.line_tokens.last().unwrap().position())
+                        .set_message("For loops must have a block")
+                        .into());
+    }
+
+    /* Iterator and range */
+
+    if errors.is_empty() && tokens[2].to_string() != "in" {
         errors.push(format_err);
     }
 
-    let iterator_name = &inner_tokens[0];
-    let mut range = parse_expression(&inner_tokens[2..], tokens[2].position());
+    let iterator_name = &tokens[1];
+    let mut range = parse_expression(&tokens[3..], tokens[3].position());
+
+    /* For loop body */
+
+    let mut block = parse_statement_block(&line.line_derivs);
+
+    /* Error recording */
 
     if let Err(ref mut es) = range {
         errors.append(es);
@@ -388,11 +370,18 @@ fn parse_for_loop<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
 }
 
 
-fn parse_return<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_return<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses a return value */
 
-    if tokens[0].to_string() == "return" {
+    let tokens = line.line_tokens;
+
+    if tokens[0].to_string() != "return" {
         return None;
+    } else if line.line_derivs.len() != 0 {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(tokens[0].position())
+                        .set_message("A return statement cannot have a block")
+                        .into());
     }
 
     let value = match &tokens[1..] {
@@ -413,8 +402,17 @@ fn parse_return<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
 }
 
 
-fn parse_expr<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
+fn parse_expr<'s, 't>(line: &Line<'s, 't>) -> ParseOption<'s, 't> {
     /* Parses a raw expression statement */
+
+    let tokens = line.line_tokens;
+
+    if line.line_derivs.len() > 0 {
+        return Some(Error::new(ErrorKind::SyntaxError)
+                        .set_position(tokens[0].position())
+                        .set_message("A raw expr statement cannot have a block")
+                        .into());
+    }
 
     Some(match parse_expression(tokens, tokens[0].position()) {
         Ok(expr) => Ok(Statement {
@@ -429,49 +427,24 @@ fn parse_expr<'s, 't>(tokens: &'t [Token<'s>]) -> ParseOption<'s, 't> {
 
 // For some reason, rust doesn't like having a list of func ptrs with lifetimes attached to them
 // as a function variable
-const PARSE_OPTIONS: [for<'s, 't> fn(&'t [Token<'s>]) -> Option<Result<Statement<'s, 't>, Vec<Error>>>; 7] = [
+const PARSE_OPTIONS: [for<'s, 't> fn(&Line<'s, 't>) -> Option<Result<Statement<'s, 't>, Vec<Error>>>; 7] = [
     parse_declare,
-    parse_assignment,
     parse_conditional,
     parse_else,
     parse_for_loop,
     parse_return,
+    parse_assignment,
     parse_expr
 ];
 
+pub fn parse_statement_block<'s, 't>(lines: &[Line<'s, 't>]) -> Result<Vec<Statement<'s, 't>>, Vec<Error>> {
+    /* Parses a block of statements */
 
-pub fn parse_block<'s, 't>(tokens: &'t [Token<'s>]) -> Result<Vec<Statement<'s, 't>>, Vec<Error>> {
-    /* Creates a list of all token slices which contains statements */
+    let mut statements = vec![];
+    let mut errors = vec![];
 
-    let mut statements = Vec::new();
-    let mut errors = Vec::new();
-
-    let slices = Token::split_tokens(tokens, |t| t.to_string() == ";" || t.delim_equals("{"));
-
-    for (i, (start, mut end)) in slices.iter().enumerate() {
-        if i + 1 == slices.len() {
-            if *start < tokens.len() {
-                errors.push(
-                    Error::new(ErrorKind::SyntaxError)
-                        .set_position(tokens[*start].position())
-                        .set_message("Statement doesn't end in a semicolon or '{'-enclosed block")
-                );
-            }
-
-            break;
-        }
-
-        if end >= tokens.len() {
-            end -= 1;
-        }
-
-        let stmt_slice = if tokens[end].atom() {
-            &tokens[*start..end]
-        } else {
-            &tokens[*start..=end]
-        };
-
-        match Statement::from_tokens(stmt_slice, tokens[*start].position()) {
+    for line in lines.iter() {
+        match Statement::from_line(line) {
             Ok(stmt) => statements.push(stmt),
             Err(ref mut es) => errors.append(es)
         }
