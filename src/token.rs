0aecad7f0c0284bd075098aaec60202233475acc
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-04-24 21:31:44 +0100

    the tokeniser now produces context-sensitive NewlineIndent variants

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,7 @@
+use crate::error::{ Error, ErrorKind };
+use lazy_static::lazy_static;
+use regex::Regex;
+
 #[derive(Clone, Debug)]
 pub enum Token<'s> {
     Identifier {
@@ -24,29 +28,28 @@ pub enum Token<'s> {
         open_delim: &'s str,
         contents: Vec<Token<'s>>,
         position: (usize, usize)
-    }
+    },
+
+    NewlineIndent(usize)
 }
 
 
 impl<'s> Token<'s> {
-    pub fn delim_equals(&self, s: &str) -> bool {
-        /* Returns whether the block's delimiter is equal to a supplied string */
-
-        if let Token::Block { open_delim, .. } = self {
-            *open_delim == s
-        } else {
-            false
-        }
-    }
-
+    /* User interface */
 
     pub fn atom(&self) -> bool {
         /* Returns whether the token is atomic */
 
-        if let Token::Block {..} = self {
-            false
-        } else {
-            true
+        if let Token::Block {..} = self { false } else { true }
+    }
+
+
+    pub fn delim(&self) -> String {
+        /* Returns whether the block's delimiter is equal to a supplied string */
+
+        match self {
+            Token::Block { open_delim, .. } => open_delim.to_string(),
+            _ => String::new()
         }
     }
 
@@ -60,6 +63,7 @@ impl<'s> Token<'s> {
             Token::Number     { position, .. } => *position,
             Token::String     { position, .. } => *position,
             Token::Symbol     { position, .. } => *position,
+            Token::NewlineIndent(_)                          => (0, 0) // irrelevant as these will be removed
         }
     }
 
@@ -97,14 +101,281 @@ impl<'s> Token<'s> {
 
 impl<'s> std::fmt::Display for Token<'s> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            Token::Identifier { string, .. } => string,
-            Token::Number     { string, .. } => string,
-            Token::String     { string, .. } => string,
-            Token::Symbol     { string, .. } => string,
-            Token::Block      {..} => ""
+        match self {
+            Token::Identifier { string, .. } => write!(f, "{}", string),
+            Token::NewlineIndent(_)                => write!(f, "{}", "<newline indent>"),
+            Token::Number     { string, .. } => write!(f, "{}", string),
+            Token::String     { string, .. } => write!(f, "{}", string),
+            Token::Symbol     { string, .. } => write!(f, "{}", string),
+            Token::Block      { open_delim, contents, ..} => {
+                let mut string = format!("{}", open_delim);
+
+                for token in contents {
+                    string += &token.to_string();
+                }
+
+                string += match *open_delim {
+                    "(" => ")",
+                    "{" => "}",
+                     _  => "]"
+                };
+            
+                write!(f, "{}", string)
+            }
+        }
+    }
+}
+
+
+/* The lexer struct
+ *
+ * This class exists to provide utility functions for the tokenise(). This is not a very clean
+ * pattern and will be removed in later versions of the compiler.
+ */
+
+struct Lexer<'f> {
+    filename: &'f str,
+    line_no: usize,
+    col_no: usize
+}
+
+impl<'f> Lexer<'f> {
+    fn build_nested<'s>(&mut self, source: &'s str, token_strings: impl Iterator<Item = &'s str>) -> Result<Vec<Token<'s>>, Vec<Error>> {
+        /* Builds nested tokens */
+        let mut token_stack = vec![
+            Token::Block {
+                open_delim:  &source[..0],
+                contents: Vec::new(),
+                position: (self.line_no, self.col_no)
+            }
+        ];
+
+        for token_string in token_strings {
+            self.match_token_string(token_string, &mut token_stack)
+                .map_err(|err| vec![ err ])?;
+        }
+
+        Ok(token_stack)
+    }
+
+
+    fn create_list_token<'s>(&mut self, open_delim: &'s str, token_stack: &mut Vec<Token<'s>>) -> Result<(), Error> {
+        /* Creates a Token::List and adds it to the token stack */
+
+        token_stack.push(Token::Block {
+            open_delim,
+            contents: Vec::new(),
+            position: (self.line_no, self.col_no)
+        });
+
+        Ok(())
+    }
+
+
+    fn create_token_strings<'s>(&self, source: &'s str) -> impl Iterator<Item = &'s str> {
+        /* Splits a source string according to a regex */
+        lazy_static! {
+            static ref REGEX: Regex = Regex::new(concat!(
+                "#.*?\n( )*|",
+                r"[a-zA-Z_][a-zA-Z0-9_]*|",
+                r"[0-9_]*[0-9]\.[0-9][0-9_]*|",
+                r"0[bB][01_]+|0[xX][0-9a-fA-F_]+|[0-9][0-9_]*|",
+                "\".*?\"|\'.*?\'|\n( )*|",
+                r"=>|\->|<=|>=|<|>|;|:|,|\.|@w\s|@|\{|\}|\(|\)|\[|\]|",
+                r"==|!=|<=|>=|\+=|\-=|\*=|/=|%=|\*\*|\++|\+|\-|\*|/|%|=|\s"
+            )).unwrap();
+        }
+
+        REGEX.find_iter(source)
+             .map(|m| m.as_str())
+             .filter(|s| s.len() > 0)
+    }
+
+
+    fn end_list_token<'s>(&mut self, close_delim: &'s str, token_stack: &mut Vec<Token<'s>>) -> Result<(), Error> {
+        /* Pushes the top of the token_stack onto the one below */
+
+        let corresponding_open_delim = match close_delim {
+            "}" => "{",
+            ")" => "(",
+            _   => "["
         };
-    
-        write!(f, "{}", s)
+
+        let stack_top = token_stack.pop().unwrap();
+        let delim = match &stack_top {
+            Token::Block { open_delim, .. } => open_delim,
+            _ => unreachable!()
+        };
+
+        if *delim != corresponding_open_delim {
+            let error_message = if *delim == "" {
+                "Found closing bracket without corresponding open bracket".into()
+            } else {
+                format!("Mismatches braces: expected '{}', received '{}", corresponding_open_delim, delim)
+            };
+
+            Error::new(ErrorKind::SyntaxError)
+                .set_filename(&self.filename)
+                .set_position((self.line_no, self.col_no))
+                .set_message(error_message)
+                .res()?
+        }
+
+        match token_stack.last_mut() {
+            Some(Token::Block { contents, .. }) => contents.push(stack_top),
+            _ => unreachable!()
+        };
+
+        Ok(())
+    }
+
+
+    fn match_general_token<'s>(&mut self, token_string: &'s str, token_stack: &mut Vec<Token<'s>>) -> Result<(), Error> {
+        /* Matches all non-delimiting tokens */
+
+        let new_token = match token_string.chars().next().unwrap() {
+            'a'..='z'|'A'..='Z'|'_' => Token::Identifier {
+                string: token_string,
+                position: (self.line_no, self.col_no)
+            },
+
+            '0'..='9' => Token::Number {
+                string: token_string,
+                position: (self.line_no, self.col_no)
+            },
+
+            '"'|'\'' => Token::String {
+                string: token_string,
+                position: (self.line_no, self.col_no)
+            },
+
+            '#'|'\n' => {
+                self.line_no += 1;
+                self.col_no   = 1;
+
+                let mut token = Token::NewlineIndent(0); // placeholder
+
+                for (i, c) in token_string.char_indices() {
+                    if c == '\n' {
+                        token = Token::NewlineIndent(token_string[i+1..].len());
+                        self.col_no = token_string[i+1..].len();
+                        break;
+                    }
+                }
+
+                token
+            }
+
+            _ => Token::Symbol {
+                string: token_string,
+                position: (self.line_no, self.col_no)
+            }
+        };
+
+        if let Token::Block { contents, .. } = token_stack.last_mut().unwrap() {
+            contents.push(new_token);
+            Ok(())
+        } else {
+            unreachable!()
+        }
+    }
+
+
+    fn match_token_string<'s>(&mut self, token_string: &'s str, token_stack: &mut Vec<Token<'s>>) -> Result<(), Error> {
+        /* Matches a token string and moves the lexer along */
+        self.col_no += token_string.len();
+
+        match token_string {
+            "{"|"("|"[" => self.create_list_token(token_string, token_stack),
+            "}"|")"|"]" => self.end_list_token(token_string, token_stack),
+            " " => Ok(()),
+            _   => self.match_general_token(token_string, token_stack)
+        }
+    }
+}
+
+pub fn tokenise<'s, 'f>(source: &'s str, filename: &'f str, pos: (usize, usize)) -> Result<Vec<Token<'s>>, Vec<Error>> {
+    /* Tokenises a source string */
+
+    let mut lexer = Lexer { filename, line_no: pos.0, col_no: pos.1 };
+    let token_strings = lexer.create_token_strings(source);
+    let mut nested_tokens = lexer.build_nested(source, token_strings)?;
+
+    match &mut nested_tokens[..] {
+        [Token::Block { contents, .. }] => {
+            for inner_token in contents.iter_mut() {
+                remove_internal_newlines(inner_token);
+            }
+
+            remove_duplicate_newlines(contents);
+            adjust_newline_step(contents);
+
+            Ok(contents.clone())
+        },
+        _ => unreachable!()
+    }
+}
+
+
+fn adjust_newline_step<'s>(tokens: &mut Vec<Token<'s>>) {
+    /* Sets the newline step to 1 in each token */
+
+    let mut newline_step = 0; // will always be a step of one
+
+    for token in tokens.iter() {
+        if let Token::NewlineIndent(indent) = token {
+            if *indent > 0 {
+                newline_step = *indent;
+                break;
+            }
+        }
+    }
+
+    if newline_step == 0 {
+        return;
+    }
+
+    for token in tokens.iter_mut() {
+        if let Token::NewlineIndent(indent) = token {
+            if *indent % newline_step == 0 {
+                *indent /= newline_step;
+            }
+        }
+    }
+}
+
+
+fn remove_duplicate_newlines<'s>(tokens: &mut Vec<Token<'s>>) {
+    /* Removes duplicate newlines in a list of tokens */
+
+    let mut is_duplicate = false;
+
+    for i in (0..tokens.len()).rev() {
+        match tokens[i] {
+            Token::NewlineIndent(_) => {
+                if is_duplicate {
+                    tokens.remove(i);
+                } else {
+                    is_duplicate = true;
+                }
+            },
+
+            _ => is_duplicate = false
+        }
+    }
+}
+
+
+fn remove_internal_newlines<'s>(token: &mut Token<'s>) {
+    /* Recursively removes newlines from nested tokens */
+
+    if let Token::Block { contents, .. } = token {
+        for i in (0..contents.len()).rev() {
+            match contents[i] {
+                Token::Block {..}       => { remove_internal_newlines(&mut contents[i]); },
+                Token::NewlineIndent(_) => { contents.remove(i); },
+                _ => {}
+            }
+        }
     }
 }
