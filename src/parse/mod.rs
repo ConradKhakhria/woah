d267dcca549a9afd562bbe614e362fabab0503aa
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-04-25 00:39:18 +0100

    added Function

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,10 +1,14 @@
 mod parse_expr;
+mod parse_function;
 mod parse_statement;
 mod parse_type;
 
 pub use parse_expr::Expr as Expr;
 pub use parse_expr::ExprType as ExprType;
 pub use parse_expr::parse_expression as parse_expression;
+
+pub use parse_function::Argument as Argument;
+pub use parse_function::Function as Function;
 
 pub use parse_statement::Statement as Statement;
 pub use parse_statement::StatementType as StatementType;
