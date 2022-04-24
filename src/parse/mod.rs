34241f4067657632aa97fd9b2f5909916e9a06d2
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-04-24 22:40:44 +0100

    added parse_statement and parse_type

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,5 +1,12 @@
 mod parse_expr;
+mod parse_statement;
+mod parse_type;
 
 pub use parse_expr::Expr as Expr;
 pub use parse_expr::ExprType as ExprType;
 pub use parse_expr::parse_expression as parse_expression;
+
+pub use parse_statement::Statement as Statement;
+pub use parse_statement::StatementType as StatementType;
+
+pub use parse_type::TypeKind as TypeKind;
