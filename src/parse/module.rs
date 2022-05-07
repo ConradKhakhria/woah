df64cc1d146643d8b6123c61e90a30c563d6c683
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-05-07 18:10:47 +0100

    created a stub for a constructor

--- src/parse/module.rs
+++ src/parse/module.rs
@@ -1,12 +1,27 @@
 use crate::{
+    error::Error,
     parse::{
         Function,
         Import
     }
 };
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    path::Path
+};
 
 pub struct Module<'s, 't> {
+    name: String,
     functions: HashMap<&'s str, Function<'s, 't>>,
     imports: Vec<Import<'s, 't>>
+}
+
+
+impl<'s, 't> Module<'s, 't> {
+    pub fn from_filepath(path: &Path) -> Result<Self, Vec<Error>> {
+        /* Reads and parses a module from a file */
+
+
+        Err(vec![])
+    }
 }
