e3ff69ed5de04ed0ff0642b60a6f26382bddfa5c
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-05-07 18:04:29 +0100

    added a module field to the import and added a temporary UnimplementedError

--- src/parse/import.rs
+++ src/parse/import.rs
@@ -1,20 +1,22 @@
 use crate::{
     error::{ Error, ErrorKind },
     line::Line,
+    parse::Module,
     token::Token
 };
 
 use derive_getters::Getters;
 
 #[derive(Getters)]
-pub struct Import<'s> {
+pub struct Import<'s, 't> {
     path: Vec<&'s str>,
     mod_name: &'s str,
+    module: Module<'s, 't>
 }
 
 
-impl<'s> Import<'s> {
-    pub fn from_line<'t>(line: &Line<'s, 't>) -> Result<Self, Vec<Error>> {
+impl<'s, 't> Import<'s, 't> {
+    pub fn from_line(line: &Line<'s, 't>) -> Result<Self, Vec<Error>> {
         /* Parses an import from a line */
 
         let path_tokens = &line.line_tokens[1..];
@@ -56,10 +58,10 @@ impl<'s> Import<'s> {
                 .set_message("An import cannot have a block")
                 .into()
         } else {
-            Ok(Import {
-                mod_name: path.last().unwrap(),
-                path
-            })
+            Error::new(ErrorKind::UnimplementedError)
+                .set_position(line.line_tokens[0].position())
+                .set_message("I haven't implemented imports yet")
+                .into()
         }
     }
 }
