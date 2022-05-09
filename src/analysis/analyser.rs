3bf44d4905453ec7e385e277a58038001128fa24
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-05-09 19:01:28 +0100

    added statement static analysis for assignments, declarations, and conditionals

--- src/analysis/analyser.rs
+++ src/analysis/analyser.rs
@@ -1,6 +1,9 @@
 use crate::{
+    analysis::get_expr_type,
     error::{ Error, ErrorKind },
     parse::{
+        Expr,
+        ExprKind,
         Function,
         Module,
         Statement,
@@ -96,20 +99,228 @@ impl<'m> Analyser<'m> {
     }
 
 
-    /* Static analysis */
+    /* Type analysis */
 
 
-    pub fn analyse_statement(&mut self, statement: &Statement) -> Vec<Error> {
-        /* Statically analyses a statement in a function */
+    fn allowable_value_type_error(&mut self, type_kind: &Rc<TypeKind>, pos: (usize, usize)) -> Option<Error> {
+       /* Determines whether values can have our type
+        *
+        * returns: an error if not
+        */
 
-        let mut error = vec![];
-    
-        match &statement.stmt_type {
-            _ => unimplemented!()
+        match &**type_kind {
+            TypeKind::EmptyList => {
+                Error::new(ErrorKind::TypeError)
+                    .set_position(pos)
+                    .set_message("The type of this array cannot be determined")
+                    .into()
+            }
+
+            TypeKind::Function {..} | TypeKind::Module(_) => {
+                Error::new(ErrorKind::TypeError)
+                    .set_position(pos)
+                    .set_message(format!("Cannot declare variables of type '{}'", type_kind))
+                    .into()
+            }
+
+            _ => None
+        }
+    }
+
+
+    /* Expression analysis */
+
+
+    fn assignable_expression_error(&mut self, expression: &Expr) -> Option<Error> {
+       /* Determines whether an expression can be assigned to
+        *
+        * returns: if not assignable, an error saying so and where
+        */
+
+        let error = Error::new(ErrorKind::TypeError).set_position(expression.first_position);
+
+        match &expression.expr_kind {
+            ExprKind::ArrayIndexing { array, .. } => {
+                self.assignable_expression_error(array)
+            }
+
+            ExprKind::ArrayLiteral { .. } => {
+                error.set_message("Cannot assign to array literal").into()
+            }
+
+            ExprKind::AttrRes { parent, attr_name } => {
+                self.assignable_expression_error(parent)
+            }
+
+            ExprKind::Compound { operator, .. } => {
+                error.set_message(format!("Cannot assign to '{}' expression", operator)).into()
+            }
+
+            ExprKind::Float(_) => error.set_message("Cannot assign to float literal").into(),
+
+            ExprKind::FunctionCall { .. } => error.set_message("Cannot assign to function call").into(),
+
+            ExprKind::Identifier(ident) => {
+                if self.current_scope.is_value_constant(ident).unwrap() {
+                    error.set_message(format!("Cannot assign to constant value '{}'", ident)).into()
+                } else {
+                    None
+                }
+            }
+
+            ExprKind::Integer(_) => error.set_message("Cannot assign to integer literal").into(),
+
+            ExprKind::String(_) => error.set_message("Cannot assign to string literal").into(),
+
+            ExprKind::Unary { operator, .. } => {
+                error.set_message(format!("Cannot assign to '{}' statement", operator)).into()
+            }
+        }
+    }
+
+
+    /* Statement analysis */
+
+
+    fn analyse_assignment(&mut self, assigned_to: &Expr, new_value: &Expr) -> Vec<Error> {
+        /* Statically analyses the types of an assignment statement */
+
+        let mut assigned_to_type = get_expr_type(self, assigned_to);
+        let mut new_value_type = get_expr_type(self, new_value);
+        let mut errors = vec![];
+
+        for result in vec![ &mut assigned_to_type, &mut new_value_type ] {
+            if let Err(ref mut es) = result {
+                errors.append(es);
+            }
         }
 
-        error
+        match (assigned_to_type, new_value_type) {
+            (Ok(att), Ok(nvt)) => {
+                if att != nvt {
+                    errors.push(Error::new(ErrorKind::TypeError)
+                                    .set_position(assigned_to.first_position)
+                                    .set_message(format!("Expected type '{}', received '{}'", att, nvt))
+                                    .into());
+                }
+
+                if let Some(e) = self.assignable_expression_error(assigned_to) {
+                    errors.push(e);
+                }
+
+                if let Some(e) = self.allowable_value_type_error(&nvt, new_value.first_position) {
+                    errors.push(e);
+                }
+            }
+
+            _ => {}
+        }
+
+        errors
     }
+
+
+    fn analyse_conditional(&mut self, condition: &Expr, block: &'m Vec<Statement>) -> Vec<Error> {
+        /* Statically analyses a conditional statement */
+
+        let mut errors = match get_expr_type(self, condition) {
+            Ok(tp) => {
+                if &*tp != &TypeKind::Bool {
+                    Error::new(ErrorKind::TypeError)
+                        .set_position(condition.first_position)
+                        .set_message("Expected boolean expression in conditional statement")
+                        .into()
+                } else {
+                    vec![]
+                }
+            }
+
+            Err(es) => es
+        };
+
+        for statement in block.iter() {
+            errors.append(&mut self.analyse_statement(statement));
+        }
+
+        errors
+    }
+
+
+    fn analyse_declaration(&mut self, statement: &'m Statement) -> Vec<Error> {
+        /* Analyses a declaration */
+
+        let (value_name, value_type, value, constant) = match &statement.stmt_type {
+            StatementType::Declare { value_name, value_type, value, constant } => {
+                (value_name, value_type, value, constant)
+            },
+
+            _ => unreachable!()
+        };
+
+        match value {
+            Some(expr) => {
+                let actual_expr_type = match get_expr_type(self, &expr) {
+                    Ok(tp) => tp,
+                    Err(es) => return es
+                };
+
+                match value_type {
+                    Some(tp) => {
+                        let errors = if &*actual_expr_type == &**tp {
+                            vec![]
+                        } else {
+                            Error::new(ErrorKind::TypeError)
+                                .set_position(expr.first_position)
+                                .set_message(format!("Expected type {}, received {}", tp, actual_expr_type))
+                                .into()
+                        };
+
+                        self.current_scope.add_value(value_name, Rc::clone(tp), *constant);
+                     
+                        errors
+                    }
+
+                    None => {
+                        match self.allowable_value_type_error(&actual_expr_type, expr.first_position) {
+                            Some(e) => e.into(),
+                            None => vec![]
+                        }
+                    }
+                }
+            }
+
+            None => {
+                Error::new(ErrorKind::UnimplementedError)
+                    .set_position(statement.first_position)
+                    .set_message("I have not implemented delayed value assignment yet")
+                    .into()
+            }
+        }
+    }
+
+
+    pub fn analyse_statement(&mut self, statement: &'m Statement) -> Vec<Error> {
+        /* Statically analyses a statement in a function */
+
+        match &statement.stmt_type {
+            StatementType::Assign { assigned_to, new_value } => {
+                self.analyse_assignment(assigned_to, new_value)
+            }
+
+            StatementType::Conditional { condition, block, .. } => {
+                self.analyse_conditional(condition, block)
+            }
+
+            StatementType::Declare { .. } => {
+                self.analyse_declaration(statement)
+            }
+
+            _ => unimplemented!()
+        }
+    }
+
+
+    /* Function analysis */
 
 
     pub fn analyse_function(&mut self, function: &'m Function) -> Vec<Error> {
@@ -130,6 +341,9 @@ impl<'m> Analyser<'m> {
 
         errors
     }
+
+
+    /* Module analysis */
 
 
     pub fn analyse_module(&mut self, module: &'m Module) -> Vec<Error> {
