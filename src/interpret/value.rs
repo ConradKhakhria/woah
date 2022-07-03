9c9073117fe05435ed9245a84d126667ed2881d2
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-04 00:03:10 +0100

    Implemented std::fmt::Display for Value

--- src/interpret/value.rs
+++ src/interpret/value.rs
@@ -23,6 +23,35 @@ pub (super) enum Value<'m> {
 }
 
 
+impl std::fmt::Display for Value<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::Array(xs) => {
+                let mut string = "[".to_string();
+
+                for x in xs.iter() {
+                    string = format!("{}{}, ", string, x.borrow());
+                }
+
+                write!(f, "{}]", &string[..string.len() - 2])
+            },
+
+            Value::Bool(x) => write!(f, "{}", x),
+
+            Value::Float(x) => write!(f, "{}", x),
+
+            Value::Function(func) => write!(f, "<function '{}'>", func.name),
+
+            Value::Int(x) => write!(f, "{}", x),
+
+            Value::NoValue => write!(f, "<no value>"),
+
+            Value::String(x) => write!(f, "{}", x)
+        }
+    }
+}
+
+
 impl<'m> Value<'m> {
     pub fn rc_refcell(self) -> Rc<RefCell<Self>> {
         /* Wraps self in an Rc */
