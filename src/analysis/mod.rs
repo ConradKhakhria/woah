2224a4b23f001b6f5f587f1c7be2e31b5df6680a
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-04 14:05:11 +0100

    added builtin.rs

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -1,6 +1,9 @@
 mod analyser;
+mod builtin;
 mod type_checking;
 
 pub use analyser::Analyser as Analyser;
+
+pub use builtin::is_builtin as is_builtin;
 
 pub use type_checking::get_expr_type as get_expr_type;
