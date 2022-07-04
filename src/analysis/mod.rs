c92b7890439865dadf9b5d1b885f4c903587c2b2
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-07-04 14:18:46 +0100

    type_of_builtin_function is now exported properly

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -4,6 +4,6 @@ mod type_checking;
 
 pub use analyser::Analyser as Analyser;
 
-pub use builtin::type_of_builtin_function as is_builtin;
+pub use builtin::type_of_builtin_function as type_of_builtin_function;
 
 pub use type_checking::get_expr_type as get_expr_type;
