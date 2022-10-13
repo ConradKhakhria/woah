9b4419f8b4a1c511372b40136ebddb70b1dcc148
Conrad Khakhria <conradjkhakhria@gmail.com>
2022-10-13 20:28:18 +0100

    Escape analysis results are no longer local to src::analysis

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -4,6 +4,6 @@ mod type_checker;
 
 pub use analyser::analyse_program as analyse_program;
 
-pub (in crate::analysis) use escape_analysis::EscapeResult as EscapeResult;
+pub use escape_analysis::EscapeResult as EscapeResult;
 
 pub (in crate::analysis) use type_checker::check_function_types as check_function_types;
